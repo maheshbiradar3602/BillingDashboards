2c684847402c62ebed1dcaf616549fb6c93a66e7
Mahesh <mahesh.biradar@credera.co.uk>
2022-02-18 13:56:15 +0100

    lamda function added

---
+++ test/Lambda/iam/lambda-iam.tf
@@ -0,0 +1,14 @@
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "test_policy"
+  role = aws_iam_role.test_role.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = "${file("iam/lambdapolicy.json")}"
+}
+
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  assume_role_policy = "${file("iam/lambda-assume-policy.json")}"
+}
